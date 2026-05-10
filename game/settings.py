9ae6d885445293bca17b91f405eb5f9171dbc6af
DecoJota <132716235+DecoJota@users.noreply.github.com>
2026-05-09 21:36:01 -0300

    segundo commit

--- game/settings.py
+++ game/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
